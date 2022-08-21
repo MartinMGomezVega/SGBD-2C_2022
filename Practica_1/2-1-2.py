84e2d0fc9473a92c7e793c5df954eafacf9271e0
MartinMGomezVega <martin_gomezvega@hotmail.com>
2022-08-21 19:56:11 -0300

    ejercicio 2-1-2 listo

--- Practica_1/2-1-2.py
+++ Practica_1/2-1-2.py
@@ -1,1 +1,21 @@
-#Tincho
+# Enunciado:
+# Dado un string con el siguiente formato: “nombre1,apellido1,DNI1/.../nombreN,apellidoN,DNIN”, escribir un programa
+# que lo procese y escriba la siguiente información por pantalla:
+# apellido1 nombre1
+# . . .
+# apellidoN nombreN
+
+import re
+
+
+def functionStringFormat():
+    str = "martin,gomez,41702705,miguel,vega,50720714"
+    matches = re.finditer('([a-zA-Z]+.),([a-zA-Z]+)+', str) # Coincide con un patrón en una cadena y devuelve un iterador que produce los Matchobjetos de todas las coincidencias que no se superponen.
+    
+    # Recorrer matches y mostrar el valor
+    for matchNum, match in enumerate(matches, start=1):
+        print (("{match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group(2)))+
+               " "+
+               "{match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group(1)))
+        
+functionStringFormat() 
