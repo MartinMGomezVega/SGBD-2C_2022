4f1496f3870956d4a363d15fefc90ccdeaa1dc49
Trinidad <trinidad.mamud-tin@bullhorn.com>
2022-08-25 00:08:59 -0300

    refactor

--- Practica_1/2-1-2.py
+++ Practica_1/2-1-2.py
@@ -9,13 +9,12 @@ import re
 
 
 def functionStringFormat():
-    str = "martin,gomez,41702705,miguel,vega,50720714"
-    matches = re.finditer('([a-zA-Z]+.),([a-zA-Z]+)+', str) # Coincide con un patrón en una cadena y devuelve un iterador que produce los Matchobjetos de todas las coincidencias que no se superponen.
+    stringExample = "martin,gomez,41702705,miguel,vega,50720714"
+    splitedStr = stringExample.split(",")
     
-    # Recorrer matches y mostrar el valor
-    for matchNum, match in enumerate(matches, start=1):
-        print (("{match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group(2)))+
-               " "+
-               "{match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group(1)))
-        
+    for word in splitedStr:
+        match = re.search('([a-zA-Z]+)', word)
+        if (bool(match)):
+            print(word)
+       
 functionStringFormat() 
