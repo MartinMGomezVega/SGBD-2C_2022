db8dd2e644c6fd191d9e096e66064e05d7a5cd3b
Trinidad <trinidad.mamud-tin@bullhorn.com>
2022-08-31 11:06:00 -0300

    arreglo de ejercicio 2-2-1 de diccionarios

--- Practica_1/2-2-1.py
+++ Practica_1/2-2-1.py
@@ -6,9 +6,9 @@
 # - Ordenar de modo descendente las palabras por cantidad de ocurrencias
 # Responder
 #   • ¿Cuántas palabras tiene el texto?
-#   • ¿Cuáles son las 5 palabras m´as usadas?
+#   • ¿Cuáles son las 5 palabras más usadas?
 
-from hashlib import new
+import operator
 import re
 
 # Archivo:
@@ -16,36 +16,33 @@ archivo = open('Practica_1/king_lear.txt')
 
 # - Pasar las palabras a minúsculas
 archivoEnMinusculas = (archivo.read()).lower()
-#print (archivoEnMinusculas)
 
 # - Descartar los signos de puntuación (sobre el archivoEnMinusculas)
 archSinSignosPuntuacion = re.sub(r'[^\w\s]','',archivoEnMinusculas)
-# print(archSinSignosPuntuacion)
 
 # - Separar y contar las ocurrencias de las palabras (Evaluar si conviene utilizar un “dict” o la clase “collections.Counter” de python)
 listaPalabras = archSinSignosPuntuacion.split()
 
-def listaPalabrasDicFrec(listaPalabras):
-    frecuenciaPalab = [listaPalabras.count(p) for p in listaPalabras]
-    return dict(list(zip(listaPalabras,frecuenciaPalab)))
-
-dictArch = listaPalabrasDicFrec(listaPalabras)
-#print (dictArch)
+def guardarPalabrasEnDict():
+    wordDict = dict()
+    for palabra in listaPalabras:
+        if (palabra in wordDict):
+            wordDict[palabra] =  wordDict.get(palabra) + 1
+        else:
+            wordDict[palabra] = 1
+    return wordDict
 
 # - Ordenar de modo descendente las palabras por cantidad de ocurrencias
-def sortDescDictionary(dictArch):
-    aux = [(dictArch[key], key) for key in dictArch]
-    aux.sort()
-    aux.reverse() #Descendente
-    return aux
+def sortDescDictionary():
+    return sorted(guardarPalabrasEnDict().items(), key=operator.itemgetter(1), reverse=True)
+    
 
-dictOrderDesc = sortDescDictionary(dictArch)
-#print(dictOrderDesc)
+dictOrderDesc = sortDescDictionary()
 
 # Responder
 #   • ¿Cuántas palabras tiene el texto?
 cantidadPalabras = len(re.findall(r'\w+', archivoEnMinusculas))
-#print("Cantidad de palabras que contiene el texto: " + str(cantidadPalabras))
+print("Cantidad de palabras que contiene el texto: " + str(cantidadPalabras))
 
 #   • ¿Cuáles son las 5 palabras más usadas?
 i = 0
