d6cb04e942aa583f43e8bd1e19e58292bc730755
MartinMGomezVega <martin_gomezvega@hotmail.com>
2022-12-01 19:51:47 -0300

    actualizado

--- Practica_4/ejercicio_3-4-3.py
+++ Practica_4/ejercicio_3-4-3.py
@@ -6,7 +6,6 @@
 
 import re
 import collections
-import random
 from matplotlib import pyplot as plt
 import pymongo as mongo
 from wordcloud import WordCloud, STOPWORDS
@@ -28,27 +27,26 @@ def convert(string):
 
 
 def obtenerTexto (pais):
-	cnt = collections.Counter()
-
-	for location in myTweets.find({"pais": pais},{"text": 1, "_id":0}):
-		texto = location.get('text')
-		#Pasar las palabras a minuscula
-		tweet = texto.lower()
-		if (":" in tweet):
-			tweet = tweet.split(":", 1)[1]
-		
-		#Descartar los signos de puntuacion
-		tweet = re.sub(r'[^a-z0-9\s]','',tweet)
-		
-		for palabraProhibida in palabrasProhibidas:
-			tweet = re.sub('\s' + palabraProhibida + '\s',' ', tweet)
-		
-		#Separar y contar las ocurrencias de las palabras
-		for word in convert(tweet):
-			cnt[word] += 1
-			
-	data = cnt.most_common(20)
-	return data
+    cnt = collections.Counter()
+    for location in myTweets.find({"pais": pais},{"text": 1, "_id":0}):
+        texto = location.get('text')
+        #Pasar las palabras a minuscula
+        tweet = texto.lower()
+        if (":" in tweet):
+            tweet = tweet.split(":", 1)[1]
+        
+        #Descartar los signos de puntuacion
+        tweet = re.sub(r'[^a-z0-9\s]','',tweet)
+        
+        for palabraProhibida in palabrasProhibidas:
+            tweet = re.sub('\s' + palabraProhibida + '\s',' ', tweet)
+        
+        #Separar y contar las ocurrencias de las palabras
+        for word in convert(tweet):
+            cnt[word] += 1
+            
+    data = cnt.most_common(20)
+    return data
 
 
 def text_size(total):
@@ -56,38 +54,34 @@ def text_size(total):
 
 #Genera la nube de palabras que va a ser graficada
 def generarNube(pais):
-	data = obtenerTexto(pais)
-	wordcloud = WordCloud(
-			background_color='white',
-			stopwords=STOPWORDS,
-			max_words=20,
-			max_font_size=40, 
-			scale=3,
-			random_state=1
-		).generate(str(data))
-	
-	return wordcloud
-	
+    data = obtenerTexto(pais)
+    wordcloud = WordCloud(
+            background_color='white',
+            stopwords=STOPWORDS,
+            max_words=20,
+            max_font_size=40, 
+            scale=4,
+            random_state=1
+        ).generate(str(data))
+    
+    return wordcloud
+    
 #Grafica las dos nubes de palabras
 def graficarNube(): 
-	pais1 = "United States"
-	pais2 = "United Kingdom"
+    paises = ["Argentina", "Brazil"]
 
-	wordcloud1 = generarNube(pais1)
-	wordcloud2 = generarNube(pais2)
+    y = 1
+    for pais in paises:
+        wordcloud = generarNube(pais)
+        
+        fig = plt.figure(1, figsize=(12, 12))
+        fig.add_subplot(1,2,y)
+        plt.imshow(wordcloud)
+        plt.title(pais)
+        plt.axis('off')
+        y += 1
 
-	fig = plt.figure(1, figsize=(12, 12))
-	fig.add_subplot(1,2,1)
-	plt.imshow(wordcloud1)
-	plt.title(pais1)
-	plt.axis('off')
-
-	fig.add_subplot(1,2,2)
-	plt.imshow(wordcloud2)
-	plt.title(pais2)
-	plt.axis('off')
-
-	plt.show()
+    plt.show()
 
 
 graficarNube()
