b4ab448d94696a890a0115f557c18a4e508ae05f
Alejandro Santari <54708794+xxalezxx@users.noreply.github.com>
2022-12-07 20:27:51 -0300

    Update ejercicio_3-4-1.py

--- Practica_4/ejercicio_3-4-1.py
+++ Practica_4/ejercicio_3-4-1.py
@@ -1,5 +1,6 @@
 
-
+ #for location in collectionTweets.find({}, {"user.location": 1, "codigo": 1, "pais": 1, "_id": 0}):
+          #  print(location)
 import pymongo
 import psycopg2
 import psycopg2.extras
@@ -15,156 +16,137 @@ import random
 import psycopg2
 
 
-MONGO_HOST="localhost"
-MONGO_PORT="27017"
-MONGO_TIEMPO_FUERA=1000
-MONGO_URI="mongodb://"+MONGO_HOST+":"+MONGO_PORT+"/"
+MONGO_HOST = "localhost"
+MONGO_PORT = "27017"
+MONGO_TIEMPO_FUERA = 1000
+MONGO_URI = "mongodb://"+MONGO_HOST+":"+MONGO_PORT+"/"
 MONGO_BASEDATOS = "test"
 MONGO_COLECCION_TWEETS = "tweets"
 
 hostname = 'localhost'
-database ='postgresDB'
+database = 'postgresDB'
 username = 'admin'
 pwd = 'admin'
 portId = '5455'
 
+
 def actualziarMongo():
     try:
-        cliente=pymongo.MongoClient(MONGO_URI,serverSelectionTimeoutMS=MONGO_TIEMPO_FUERA)
-        baseDatos=cliente[MONGO_BASEDATOS]
-        coleccion_tw=baseDatos[MONGO_COLECCION_TWEETS]
-        baseDatos=cliente[MONGO_BASEDATOS]
-        collection = baseDatos[MONGO_COLECCION_TWEETS]
-        
+
+        cliente = pymongo.MongoClient(
+            MONGO_URI, serverSelectionTimeoutMS=MONGO_TIEMPO_FUERA)
+        baseDatos = cliente[MONGO_BASEDATOS]
+        collectionTweets = baseDatos[MONGO_COLECCION_TWEETS]
+
         cliente.server_info()
+
         print("BD Mongo OK!")
+
     except:
         print("BD Mongo : FAIL!")
-        
-    cursor = collection.find({})
 
-    
-    for i in cursor:
-        _id = i['id']
-        userLocation = i['user']['location']
-        print("ID: ",i['user']['id'])
-        print("-> ",userLocation)
-        value = "No information"
-        if (userLocation != None):
-            value = obtenerCodigoPais(userLocation)
-        query = {"id": _id}
-        print(value)
-        newValues = {"$set": {'countryCode': value}}
-        collection.update_one(query, newValues)
+    # Creacion del campo codigo
+    collectionTweets.update_many({}, {"$set": {"user.codigo": "null"}})
+    collectionTweets.update_many({}, {"$set": {"user.pais": "null"}})
 
-def obtenerCodigoPais(userLocation):
-    diccionarioCitiesNameCountry = devolverDiccionario(
-        'SELECT name, countrycode FROM city')
-    diccionarioCitiesDistrictCountry = devolverDiccionario(
-        'SELECT district, countrycode FROM city')
-    diccionarioCountries = devolverDiccionario(
-        'SELECT name, code FROM country')
-    value = 'X'
-
-    for i in diccionarioCountries.keys():
-            if (userLocation in i or i in userLocation):
-                value = diccionarioCountries[i]
-                break
-            
-    if (value == 'X'):
-        for i in diccionarioCitiesNameCountry.keys():
-            if (userLocation in i or i in userLocation):
-                value = diccionarioCitiesNameCountry[i]
-                break
-    
-    if (value == 'X'):
-        for i in diccionarioCitiesDistrictCountry.keys():
-            if (userLocation in i or i in userLocation):
-                value = diccionarioCitiesDistrictCountry[i]
-                break
-    
-    if (value == 'X'):
-        print("Nada disponible")
-
-    return value
-
-def conectarBD():
     try:
-        conexion = psycopg2.connect(
-            user = username, 
-            password = pwd, 
-            host = hostname, 
-            port = portId, 
-            database = "postgresDB")
+        conexionPostgres = psycopg2.connect(
+            user=username,
+            password=pwd,
+            host=hostname,
+            port=portId,
+            database="postgresDB")
+        
+        print("BD Postgres OK!")
 
     except (Exception, psycopg2.Error) as error:
-        print ("BD postgres : FAIL!", error)
+        print("BD postgres : FAIL!", error)
+
+    cursor = conexionPostgres.cursor()
+
+    countryQuery = 'SELECT name, code, localname FROM country order by name;'
+    cursor.execute(countryQuery)
+    countryData = cursor.fetchall()
+
+    cityQuery = "select city.name, city.countrycode, country.name from city inner join country on city.countrycode = country.code;"
+    cursor.execute(cityQuery)
+    cityData = cursor.fetchall()
+
+    tweetsCol = collectionTweets.find(
+        {}, {"user.location": 1, "codigo": 1, "_id": 0})
+
+    for location in tweetsCol:
+
+        codigo = ""
+        pais = ""
+        state = False
+
+        localizacion = location.get("user").get("location")
+
         
-    return conexion
+        if(localizacion != None):
+            if ('THE UNITED STATES OF AMERICA' in localizacion or 'NY' in localizacion or 'U.S.A' in localizacion or 'Arizona' in localizacion or 'TX' in localizacion or 'Florida' in localizacion or 'Texas' in localizacion):
+                state = True
+                codigo = 'USA'
+                pais = 'United States'
+                
+            elif ('CABA' in localizacion or 'Buenos Aire' in localizacion or 'BS.AS' in localizacion):
+                state = True
+                codigo = 'ARG'
+                pais = 'Argentina'
+                
+            elif (state == False):
+                
+                for row in countryData:
+                    if (row[0] in localizacion and state == False):
+                        state = True
+                        codigo = row[1]
+                        pais = row[0]
+                        break
+                    
+                    elif (row[1] in localizacion and state == False):
+                        state = True
+                        codigo = row[1]
+                        pais = row[0]
+                        break
+                    
+                    elif (row[2] in localizacion and state == False):
+                        state = True
+                        codigo = row[1]
+                        pais = row[0]
+                        break
 
-
-def crearDiccionario(query):
-    conexion = conectarBD()
-    cur = conexion.cursor()
-    cur.execute(query)
-    fila = cur.fetchone()
-    diccionario = {}
-    
-    while fila is not None:
-            clave = fila[0]
-            valor = fila[1] 
-            diccionario[clave] = valor
-            fila = cur.fetchone()
-
-    cur.close()
-    cerrarConexion(conexion)
-
-    return diccionario
-
-def devolverDiccionario(query):
-    diccionario = crearDiccionario(query)
-    return diccionario
-
-def cerrarConexion(conexion):
-    if(conexion):
-        conexion.close()
-    else:
-        print("Close conection FAIL!")
-        
-
-def graficarMapaTweets():
-    try:
-    
-        cliente=pymongo.MongoClient(MONGO_URI,serverSelectionTimeoutMS=MONGO_TIEMPO_FUERA)
-        baseDatos=cliente[MONGO_BASEDATOS]
-        coleccion_tw=baseDatos[MONGO_COLECCION_TWEETS]
-        baseDatos = cliente.test
-        collection = baseDatos['tweets']
-        print("BD Mongo OK!")
-    except:
-        print("BD Mongo : FAIL!")
-    cursor = collection.find({})
-    values = []
-    dicPaises = devolverDiccionario(
-        'SELECT name, code FROM country')
-    diccionario = {}
-    for idx, i in enumerate(cursor):    
-        if("countryCode" in i.keys() and idx < 50000):
-            values.append(i['countryCode'])
-
-    for i in dicPaises.keys():
-        diccionario[dicPaises[i]] = values.count(dicPaises[i])
-
-    print("Progresando....")
-    dataFrame = pd.DataFrame(list(diccionario.items()), columns=['codigoPais', 'cantidad'])
- 
-    fig = px.choropleth(dataFrame, locations='codigoPais', color='cantidad', scope="world")
-    fig.show()
+                if (state == False):
+                    for fila in cityData:
+                        if (fila[0] in localizacion):
+                            state = True
+                            codigo = fila[1]
+                            pais = fila[2]
+                            
+        nuevoValores = {"$set": 
+            {
+                "user.codigo": codigo, 
+                "user.pais": pais
+            }
+        }
+        updates = collectionTweets.update_many(
+            {
+                "user.location": localizacion
+            }
+            , nuevoValores)
         
         
+        print("*******-user location: ",localizacion)
+        print("__pais: ",pais)
+        print("__codigo: ",codigo)
+        
+        #print(updates)
+       
+
+
 def main():
     actualziarMongo()
-    #graficarMapaTweets()
+    # graficarMapaTweets()
 
-    
+
 main()
